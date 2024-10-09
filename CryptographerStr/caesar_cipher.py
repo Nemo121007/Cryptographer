c35e94654647a41549147657af9622160ebffb1d
Gashkov Saveliy <120210549+Nemo121007@users.noreply.github.com>
2024-10-10 00:47:02 +0300

    Первый тест

--- CryptographerStr/caesar_cipher.py
+++ CryptographerStr/caesar_cipher.py
@@ -1,11 +1,2 @@
-def add_method(cls):
-    def decorator(func):
-        setattr(cls, func.__name__, func)
-        return func
-    return decorator
 
-
-@add_method(str)
-def caesar_cipher_decryption(self):
-    return ' '.join(reversed(self.split()))
 
